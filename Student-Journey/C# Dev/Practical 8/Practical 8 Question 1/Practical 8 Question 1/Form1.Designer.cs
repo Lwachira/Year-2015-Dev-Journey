4b8b89c050b432cd31d3e986ffedbca8bc024d55
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-07-14 16:34:09 +0200

    Added the dll

--- Student-Journey/C# Dev/Practical 8/Practical 8 Question 1/Practical 8 Question 1/Form1.Designer.cs
+++ Student-Journey/C# Dev/Practical 8/Practical 8 Question 1/Practical 8 Question 1/Form1.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtStudName = new System.Windows.Forms.TextBox();
+            this.txtStudNumber = new System.Windows.Forms.TextBox();
+            this.txtStudentDegree = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAcademic = new System.Windows.Forms.Button();
+            this.btnArts = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtStudName
+            // 
+            this.txtStudName.Location = new System.Drawing.Point(12, 26);
+            this.txtStudName.Name = "txtStudName";
+            this.txtStudName.Size = new System.Drawing.Size(100, 20);
+            this.txtStudName.TabIndex = 0;
+            // 
+            // txtStudNumber
+            // 
+            this.txtStudNumber.Location = new System.Drawing.Point(12, 52);
+            this.txtStudNumber.Name = "txtStudNumber";
+            this.txtStudNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtStudNumber.TabIndex = 1;
+            // 
+            // txtStudentDegree
+            // 
+            this.txtStudentDegree.Location = new System.Drawing.Point(12, 78);
+            this.txtStudentDegree.Name = "txtStudentDegree";
+            this.txtStudentDegree.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentDegree.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Student Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Student Degree";
+            // 
+            // btnAcademic
+            // 
+            this.btnAcademic.Location = new System.Drawing.Point(37, 177);
+            this.btnAcademic.Name = "btnAcademic";
+            this.btnAcademic.Size = new System.Drawing.Size(75, 23);
+            this.btnAcademic.TabIndex = 6;
+            this.btnAcademic.Text = "Academic";
+            this.btnAcademic.UseVisualStyleBackColor = true;
+            // 
+            // btnArts
+            // 
+            this.btnArts.Location = new System.Drawing.Point(162, 177);
+            this.btnArts.Name = "btnArts";
+            this.btnArts.Size = new System.Drawing.Size(75, 23);
+            this.btnArts.TabIndex = 7;
+            this.btnArts.Text = "Arts";
+            this.btnArts.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Chose the society your in.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnArts);
+            this.Controls.Add(this.btnAcademic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStudentDegree);
+            this.Controls.Add(this.txtStudNumber);
+            this.Controls.Add(this.txtStudName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStudName;
+        private System.Windows.Forms.TextBox txtStudNumber;
+        private System.Windows.Forms.TextBox txtStudentDegree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAcademic;
+        private System.Windows.Forms.Button btnArts;
+        private System.Windows.Forms.Label label4;
     }
 }
 
