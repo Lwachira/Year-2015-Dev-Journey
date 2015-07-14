95f80b1f5d1449471af7745ad5a6437e126129ae
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-07-14 19:06:29 +0200

    Finished Students Form

--- Student-Journey/C# Dev/Practical 8/Practical 8 Question 2/Practical 8 Question 2/Form1.Designer.cs
+++ Student-Journey/C# Dev/Practical 8/Practical 8 Question 2/Practical 8 Question 2/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(88, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(499, 160);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 383);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Students";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +54,7 @@
         }
 
         #endregion
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Student-Journey/C# Dev/Practical 8/Practical 8 Question 2/Practical 8 Question 2/Form1.cs
+++ Student-Journey/C# Dev/Practical 8/Practical 8 Question 2/Practical 8 Question 2/Form1.cs
@@ -7,17 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Societies;
 
 namespace Practical_8_Question_2
 {
+
+
     public partial class Form1 : Form
     {
+
+        Student[] students = new Student[100];
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader myStudentReader = new StreamReader("student.txt");
+            string line;
+            string[] words = new string[4];
+            int counter = 0;
+            while (!myStudentReader.EndOfStream)
+            {
+                line = myStudentReader.ReadLine();
+                words = line.Split(',');
+
+                students[counter] = new Student(words[0], words[1], words[2]);
+                counter = counter + 1;
+
+
+            }
+            for (int i = 1; i < counter; i++)
+            {
+                listBox1.Items.Add(students[].ToString());
+            }
+           
+
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
