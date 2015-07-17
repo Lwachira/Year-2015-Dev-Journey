e02d586dcc4f74bbb60c464a18fbd1e661d50839
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-07-17 21:14:54 +0200

    New lesson

---
+++ Student-Journey/Asp.Net/Ecard/Ecard/Ecard.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ecard
+{
+    public partial class Ecard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                lstBackColor.Items.Add("White");
+                lstBackColor.Items.Add("Red");
+                lstBackColor.Items.Add("Green");
+                lstBackColor.Items.Add("Blue");
+                lstBackColor.Items.Add("Yellow");
+
+                lstFontName.Items.Add("Times New Roman");
+                lstFontName.Items.Add("Arial");
+                lstFontName.Items.Add("Verdana");
+                lstFontName.Items.Add("Tahoma");
+
+                ListItem item = new ListItem();
+                item.Text = BorderStyle.None.ToString();
+
+            }
+        }
+
+        protected void cmdUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
