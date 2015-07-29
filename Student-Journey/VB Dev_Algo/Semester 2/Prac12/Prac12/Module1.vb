166e6b1a1c100fc42387e524be366cdd44b5be52
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-07-29 10:10:24 +0200

    updates

--- Student-Journey/VB Dev_Algo/Semester 2/Prac12/Prac12/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/Prac12/Prac12/Module1.vb
@@ -1,7 +1,24 @@
 ﻿Module Module1
 
     Sub Main()
+        Console.Title = "Prac 12"
+        Dim currentRow As String()
+        Dim holdNumber As String
 
+        Using myReader As New FileIO.TextFieldParser("Marks.txt")
+            myReader.TextFieldType = FileIO.FieldType.Delimited
+            myReader.SetDelimiters("#")
+            currentRow = myReader.ReadFields()
+            holdNumber = currentRow(0)
+            PrintHead()
+            While Not myReader.EndOfData
+
+            End While
+        End Using
+        Console.ReadLine()
     End Sub
 
+    Private Sub PrintHead()
+        Console.WriteLine("Student# " + " Student Name")
+    End Sub
 End Module
