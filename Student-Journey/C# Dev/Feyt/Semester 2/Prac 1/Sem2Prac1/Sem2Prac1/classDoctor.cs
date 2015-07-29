cd7d0c4df6f1c7c0a674e87dd94cd8732f882aae
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-07-29 16:15:35 +0200

    Updates

---
+++ Student-Journey/C# Dev/Feyt/Semester 2/Prac 1/Sem2Prac1/Sem2Prac1/classDoctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem2Prac1
+{
+    class classDoctor
+    {
+
+        private string doctorID;
+        private string doctorSurnam;
+        private string doctorFirstName;
+        private string doctorCellNo;
+
+
+    }
+}
