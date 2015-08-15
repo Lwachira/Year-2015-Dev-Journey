bba02be855d9711bb5c9c0fc6b1da864a86e9372
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-08-15 18:08:22 +0200

    Update 2

--- Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 12/Prac 12/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 12/Prac 12/Module1.vb
@@ -1,7 +1,29 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim currentRow As String()
+        Dim subTotal As Integer
+        Dim average As Double
+        Dim studentCount As Integer = 1
 
+        Try
+            Using myReader As New FileIO.TextFieldParser("Marks.txt")
+                myReader.TextFieldType = FileIO.FieldType.Delimited
+                myReader.SetDelimiters("#")
+                currentRow = myReader.ReadFields()
+                PrintHead()
+                subTotal += Integer.Parse(currentRow(3)) + Integer.Parse(currentRow(4)) + Integer.Parse(currentRow(5)) + Integer.Parse(currentRow(6))
+
+                average = subTotal / studentCount
+            End Using
+        Catch ex As ArgumentException
+
+        Finally
+            Console.ReadLine()
+        End Try
     End Sub
 
+    Private Sub PrintHead()
+        Console.WriteLine("Student# " + vbTab + "Student Name".PadRight(20) + "Mark 1 " + vbTab + " Mark 2 " + vbTab + " Mark 3 " + vbTab + " Mark 4 ")
+    End Sub
 End Module
