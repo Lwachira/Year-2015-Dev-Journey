1b5c2c35d41ebf79ac4f9b5c21cba074dd6bd303
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-08-15 16:07:35 +0200

    Update 1 - Created a Welcome Screen

---
+++ Student-Journey/C# Dev/Feyt/Semester 2/Project/BayWest/BayWest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BayWest
+{
+    public partial class frmWelcome : Form
+    {
+        public frmWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWelcome_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void frmWelcome_Click(object sender, EventArgs e)
+        {
+            Home myHome = new Home();
+
+            this.Visible = false;
+            myHome.ShowDialog();
+            this.Visible = true;
+        }
+    }
+}
