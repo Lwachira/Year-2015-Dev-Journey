092112a2d00519b66ee7a6f2b6cc193b2121459b
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-08-24 13:00:09 +0200

    Updates

--- Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 15_A/Prac 15_A/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 15_A/Prac 15_A/Module1.vb
@@ -18,9 +18,10 @@ Module Module1
             total = Integer.Parse(currentRow(2))
             highest = -1
             highest = Math.Max(total, highest)
+
+            PrintReportHeading()
             While Not myReader.EndOfData
                 currentRow = myReader.ReadFields()
-
                 If holdCity <> currentRow(0) Then
 
                 End If
@@ -29,6 +30,22 @@ Module Module1
 
         Console.ReadLine()
 
+    End Sub
+
+    Private Sub PrintReportHeading()
+        Console.WriteLine("RAINFALL REPORT")
+        Console.WriteLine("===============")
+    End Sub
+    Private Sub PrintSectionHeading(city As String)
+        Console.WriteLine(city.ToUpper.PadRight(16, " ") + vbTab + "Rainfall (mm)")
+    End Sub
+
+    Private Sub PrintLine(cityDate As String, rainFall As String)
+        Console.WriteLine(cityDate.PadRight(16, " ") + vbTab + rainFall)
+    End Sub
+
+    Private Sub PrintSectionFooter()
+        Console.WriteLine("Total: ".PadRight())
     End Sub
 
 End Module
