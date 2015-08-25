274c5c38f90b16456e0e7c1cb73c4438a8fc72b9
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-08-25 17:32:19 +0200

    Updates

--- Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 15_A/Prac 15_A/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/Test Revison/Prac 15_A/Prac 15_A/Module1.vb
@@ -20,12 +20,24 @@ Module Module1
             highest = Math.Max(total, highest)
 
             PrintReportHeading()
+            PrintSectionHeading(holdCity)
+            PrintLine(currentRow(1), currentRow(2))
             While Not myReader.EndOfData
                 currentRow = myReader.ReadFields()
-                If holdCity <> currentRow(0) Then
 
+                If holdCity <> currentRow(0) Then
+                    PrintSectionFooter(total)
+                    highest = Math.Max(total, highest)
+                    total = 0
+                    holdCity = currentRow(0)
+                    PrintSectionHeading(holdCity)
                 End If
+                PrintLine(currentRow(1), currentRow(2))
+                total += Integer.Parse(currentRow(2))
             End While
+            highest = Math.Max(total, highest)
+            PrintSectionFooter(total)
+            PrintReportFooter(highest)
         End Using
 
         Console.ReadLine()
@@ -44,8 +56,13 @@ Module Module1
         Console.WriteLine(cityDate.PadRight(16, " ") + vbTab + rainFall)
     End Sub
 
-    Private Sub PrintSectionFooter()
-        Console.WriteLine("Total: ".PadRight())
+    Private Sub PrintSectionFooter(total As Integer)
+        Console.WriteLine("Total: ".PadRight(16, " ") + vbTab + "{0}", total)
+        Console.WriteLine()
+    End Sub
+
+    Private Sub PrintReportFooter(highest As Integer)
+        Console.WriteLine("Highest recorded total rainfall:  {0}", highest)
     End Sub
 
 End Module
