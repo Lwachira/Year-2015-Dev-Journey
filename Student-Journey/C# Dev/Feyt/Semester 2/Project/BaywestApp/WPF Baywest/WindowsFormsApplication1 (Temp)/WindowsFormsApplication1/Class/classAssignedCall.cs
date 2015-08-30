ae51c5188a48a0945573bbba9d09fa6974c72436
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-08-30 13:44:41 +0200

    Updates

--- Student-Journey/C# Dev/Feyt/Semester 2/Project/BaywestApp/WPF Baywest/WindowsFormsApplication1 (Temp)/WindowsFormsApplication1/Class/classAssignedCall.cs
+++ Student-Journey/C# Dev/Feyt/Semester 2/Project/BaywestApp/WPF Baywest/WindowsFormsApplication1 (Temp)/WindowsFormsApplication1/Class/classAssignedCall.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,95 @@ namespace WindowsFormsApplication1.Class
 {
     internal class classAssignedCall
     {
-        //private int assignedCallID;
-        //private int callID;
-        //private int staffID;
-        //private string shopID;
+        private BusinessLayer bl = new BusinessLayer();
+        private int assignedCallID;
+        private int callID;
+        private int staffID;
+        private string startDate;
+        private int shopID;
+        private string callStatus;
+        private int serviceID;
+
+        public classAssignedCall(int assignedCallID, int callID, int staffID, string startDate, int shopID, string callStatus, int serviceID)
+        {
+            this.assignedCallID = assignedCallID;
+            this.callID = callID;
+            this.staffID = staffID;
+            this.startDate = startDate;
+            this.shopID = shopID;
+            this.callStatus = callStatus;
+            this.serviceID = serviceID;
+        }
+
+        public classAssignedCall(int assignedCallID)
+        {
+            this.assignedCallID = assignedCallID;
+        }
+
+        public classAssignedCall()
+        {
+        }
+
+        public int AssignedCall
+        {
+            get { return assignedCallID; }
+            set { assignedCallID = value; }
+        }
+
+        public int CallID
+        {
+            get { return callID; }
+            set { callID = value; }
+        }
+
+        public int StaffID
+        {
+            get { return staffID; }
+            set { staffID = value; }
+        }
+
+        public string StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public int ShopID
+        {
+            get { return shopID }
+            set { shopID = value; }
+        }
+
+        public string CallStatus
+        {
+            get { return callStatus; }
+            set { callStatus = value; }
+        }
+
+        public int ServiceID
+        {
+            get { return serviceID; }
+            set { serviceID = value; }
+        }
+
+        public DataTable GetAllLogin()
+        {
+            return bl.GetAllLogin();
+        }
+
+        public int InsertLogin()
+        {
+            return bl.InsertLogin(this);
+        }
+
+        public int DeleteLogin()
+        {
+            return bl.DeleteLogin(this);
+        }
+
+        public int UpdateLogin()
+        {
+            return bl.UpdateLogin(this);
+        }
     }
 }
