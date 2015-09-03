863a6432bc44eb74799e3d2c61291ebb204b1a37
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-09-03 20:25:27 +0200

    Prac 16 V2 - Update 1

--- Student-Journey/VB Dev_Algo/Semester 2/Prac 16V2/Prac 16V2/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/Prac 16V2/Prac 16V2/Module1.vb
@@ -1,8 +1,16 @@
-﻿Module Module1
+﻿Imports Microsoft.VisualBasic.FileIO
+Module Module1
 
     Sub Main()
+        Using myReader As New FileIO.TextFieldParser("2015 Sales.txt")
+            PrintHeading()
 
-        Console.ReadLine()
+            Console.ReadLine()
+    End Sub
+
+    Private Sub PrintHeading()
+        Console.WriteLine("Nelson Mandela Bay Area - Spar Annual Sales Report")
+        Console.WriteLine("===================================================")
     End Sub
 
 End Module
