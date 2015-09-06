4d2b13ecf8953952281dc742ef8101a95608695d
Lincoln <blazeboy.wachira@gmail.com>
2015-09-06 16:11:47 +0200

    Need to make a final section

--- Student-Journey/VB Dev_Algo/Semester 2/PRT2000 - Project/s213200619-PRT Assignment/s213200619-PRT Assignment/Module1.vb
+++ Student-Journey/VB Dev_Algo/Semester 2/PRT2000 - Project/s213200619-PRT Assignment/s213200619-PRT Assignment/Module1.vb
@@ -8,8 +8,7 @@ Module Module1
         Dim currentRow As String()
         Dim holdFaculty As String
         Dim holdDepart As String
-        Dim studNum As String
-        Dim studName As String
+
         Dim studentCount As Integer = 0
 
         Using myReader As New FileIO.TextFieldParser("students.txt")
@@ -18,20 +17,51 @@ Module Module1
             currentRow = myReader.ReadFields()
             holdFaculty = currentRow(0)
             holdDepart = currentRow(1)
-            studNum = currentRow(2)
-            studName = currentRow(3)
+
             PrintCourseHeading(holdFaculty)
             PrintDepartmentHeading(holdDepart)
-            PrintStudentline(studNum, studName)
+            PrintStudentline(currentRow(2), currentRow(3))
             studentCount += 1
             While Not myReader.EndOfData
+                currentRow = myReader.ReadFields()
 
+                If holdFaculty <> currentRow(0) Then
+
+
+                    PrinttotalStudents(studentCount, holdDepart)
+                    PrintCourseHeading(currentRow(0))
+
+                    PrintDepartmentHeading(currentRow(1))
+                    holdDepart = currentRow(1)
+                    holdFaculty = currentRow(0)
+                    studentCount = 0
+                ElseIf holdDepart <> currentRow(1)
+                    PrinttotalStudents(studentCount, holdDepart)
+                    PrintDepartmentHeading(currentRow(1))
+
+                    holdDepart = currentRow(1)
+                    studentCount = 0
+                End If
+
+                'Always has to happen
+                PrintStudentline(currentRow(2), currentRow(3))
+                studentCount += 1
             End While
-
+            PrinttotalStudents(studentCount, holdDepart)
         End Using
 
         Console.ReadLine()
     End Sub
+
+    Private Sub PrinttotalStudents(studCount As Integer, holdDepartment As String)
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("There are {0} students in {1} ", studCount.ToString(), holdDepartment)
+        Console.WriteLine()
+
+        Console.ResetColor()
+    End Sub
+
 
     Private Sub PrintStudentline(studNum As String, studName As String)
         Console.WriteLine(studNum + vbTab + studName)
@@ -39,12 +69,15 @@ Module Module1
 
     Private Sub PrintDepartmentHeading(holdDepart As String)
         Console.ForegroundColor = ConsoleColor.Yellow
+
         Console.WriteLine("Department of: " + holdDepart)
+
         Console.ResetColor()
     End Sub
 
     Private Sub PrintCourseHeading(holdFaculty As String)
         Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine()
         Console.WriteLine("School of: " + holdFaculty)
         Console.WriteLine("---------------")
         Console.ResetColor()
@@ -53,6 +86,5 @@ Module Module1
     Private Sub PrintHeading()
         Console.WriteLine("NMMU Registered Students and Course")
         Console.WriteLine("-----------------------------------")
-        Console.WriteLine()
     End Sub
 End Module
