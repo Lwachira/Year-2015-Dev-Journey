f1eddda95c0641e8c63f15ac072ea6351f70857d
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-09-23 20:31:48 +0200

    Done

--- Student-Journey/Asp.Net/Andrew/Styles And Themes/Prac 02/Prac 02/Site1.Master.designer.cs
+++ Student-Journey/Asp.Net/Andrew/Styles And Themes/Prac 02/Prac 02/Site1.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Prac_02 {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
         /// Menu1 control.
         /// </summary>
         /// <remarks>
@@ -49,12 +58,12 @@ namespace Prac_02 {
         protected global::System.Web.UI.WebControls.Menu Menu1;
         
         /// <summary>
-        /// SiteMapDataSource1 control.
+        /// SiteMapPath1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
     }
 }
