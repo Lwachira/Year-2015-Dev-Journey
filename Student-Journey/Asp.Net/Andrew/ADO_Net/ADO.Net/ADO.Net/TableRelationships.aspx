141f568d93e702a86c72e03dd5f09c7596594eb7
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-09-29 23:14:57 +0200

    Updates

---
+++ Student-Journey/Asp.Net/Andrew/ADO_Net/ADO.Net/ADO.Net/TableRelationships.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TableRelationships.aspx.cs" Inherits="ADO.Net.TableRelationships" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label EnableViewState="false" ID="lblList" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Student-Journey/Asp.Net/Andrew/ADO_Net/ADO.Net/ADO.Net/TableRelationships.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO.Net
+{
+    public partial class TableRelationships : System.Web.UI.Page
+    {
+        private string connctionString = WebConfigurationManager.ConnectionStrings["Pubs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                CreateList();
+            }
+        }
+
+        private void CreateList()
+        {
+            string sql = "Select au_lname,au_fname,au_id From Authors";
+            SqlConnection con = new SqlConnection(connctionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter dbAdapter = new SqlDataAdapter(cmd);
+            DataSet dsPubs = new DataSet();
+        }
+    }
+}
