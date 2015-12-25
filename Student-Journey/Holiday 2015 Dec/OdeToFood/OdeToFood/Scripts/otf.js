b0b13ee829a0e6af669c0b1d3240d5714a427625
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-12-25 18:36:00 +0200

    updates
    Just did some updates on bundle config

--- Student-Journey/Holiday 2015 Dec/OdeToFood/OdeToFood/Scripts/otf.js
+++ Student-Journey/Holiday 2015 Dec/OdeToFood/OdeToFood/Scripts/otf.js
@@ -2,17 +2,28 @@
 
     var ajaxFormSubmit = function () {
         var $form = $(this);
+
         var options = {
             url: $form.attr("action"),
             type: $form.attr("method"),
             data: $form.serialize()
         };
+
         $.ajax(options).done(function (data) {
             var $target = $($form.attr("data-otf-target"));
-            $target.replace(data);
+            $target.replaceWith(data);
         });
         return false;
     };
 
-    $("form[data-otf-ajax='true']".submit(ajaxFormSubmit))
+    var createAutocomplete = function () {
+        var $input = $(this);
+        var options = {
+            sources: $input.attr("data-otf-autocomplete")
+        };
+        $input.autocomplete(options);
+    }
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+    $("input[data-otf-autocomplete]").each(createAutocomplete);
 });
