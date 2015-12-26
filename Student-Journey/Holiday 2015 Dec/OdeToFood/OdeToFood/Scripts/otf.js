fd659e26f5ec98a39299c52c80dc7ac406d12c43
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-12-26 21:35:39 +0200

    Updates

--- Student-Journey/Holiday 2015 Dec/OdeToFood/OdeToFood/Scripts/otf.js
+++ Student-Journey/Holiday 2015 Dec/OdeToFood/OdeToFood/Scripts/otf.js
@@ -16,11 +16,20 @@
         return false;
     };
 
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.lable);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
+    };
+
     var createAutocomplete = function () {
         var $input = $(this);
 
         var options = {
-            sources: $input.attr("data-otf-autocomplete") 
+            sources: $input.attr("data-otf-autocomplete"),
+            select : submitAutocompleteForm
         };
         $input.autocomplete(options);
     };
