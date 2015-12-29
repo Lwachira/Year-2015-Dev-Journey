3815d99ae2481f6cfb599b4a6ab805df6056198d
Whiteshadow12 <blazeboy.wachira@gmail.com>
2015-12-30 01:55:57 +0200

    Updates

---
+++ Student-Journey/Holiday 2015 Dec/OdeToFood/OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OdeToFood.Models;
+
+namespace OdeToFood.Tests.Features
+{
+    internal class RestaurantRater
+    {
+        private Restaurant data;
+
+        public RestaurantRater(Restaurant data)
+        {
+            this.data = data;
+        }
+
+     public RatingResult ComputeRating(int v)
+        {
+            var result = new RatingResult();
+            result.Rating = 4;
+            return result;
+
+        }
+    }
+}
